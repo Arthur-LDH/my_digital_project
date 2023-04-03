513df668367c4c81bef351df9b66b0038888a0cf
Alex Otoktone <alexandre.desmot@gmail.com>
2023-04-03 13:39:26 +0200

    login: update login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,7 +18,7 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
+        return $this->render('login/login.html.twig', [
             'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
             'error'         => $error,
