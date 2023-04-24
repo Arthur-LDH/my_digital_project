04735ce71cfbedf1e5e16442e4feba4919eb3499
Arthur <arthurldh@gmail.com>
2023-04-24 11:18:44 +0200

    filters

--- src/Form/FoodCategoryType.php
+++ src/Form/FoodCategoryType.php
@@ -2,20 +2,46 @@
 
 namespace App\Form;
 
-use App\Entity\FoodCategory;
+use App\Entity\Filter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class FoodCategoryType extends AbstractType
+class UserFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', TextType::class, [
-                'label' => 'Nom de votre catégorie',
-                'placeholder' => 'Nom de votre catégorie'
+                'required' => false,
+                'label' => 'Rechercher',
+                'attr' => [
+                    'placeholder' => 'Rechercher', 
+                ],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'required' => false,
+                'label' => 'Filtrer par role',
+                'attr' => [
+                    'placeholder' => 'Filtrer par role',
+                ],
+                'choices'  => [
+                    'Utilisateur' => '[]',
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+            ])
+            ->add('isVerified', ChoiceType::class, [
+                'required' => false,
+                'label' => 'Utilisateur vérifié',
+                'attr' => [
+                    'placeholder' => 'Filtrer par role',
+                ],
+                'choices'  => [
+                    'Vérifié' => '1',
+                    'Non vérifié' => '0'
+                ],
             ])
         ;
     }
@@ -23,7 +49,14 @@ class FoodCategoryType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => FoodCategory::class,
+            'data_class' => Filter::class,
+            'method' => 'get',
+            'csrf_protection' => false,
         ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
     }
 }
