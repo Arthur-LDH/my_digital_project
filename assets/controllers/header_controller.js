0168b01462bc8b63be680a5772c4f0336aa940e4
Arthur <arthurldh@gmail.com>
2023-04-25 09:06:45 +0200

    fix: header-btn

--- assets/controllers/header_controller.js
+++ assets/controllers/header_controller.js
@@ -1,22 +1,11 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['sidebar'];
-    isOpen = false;
-
     connect() {
-        const header = document.getElementById('header-menu');
+        const header = document.getElementById('header-btn');
+        const sidebar = document.getElementById('sidebar');
         header.addEventListener('click', event => {
-            this.isOpen = !this.isOpen;
-            this.toggleSidebar();
+            sidebar.classList.toggle('active');
         })
-    }
-
-    toggleSidebar() {
-        if (this.isOpen) {
-            this.sidebarTarget.classList.add('active');
-        } else {
-            this.sidebarTarget.classList.remove('active');
-        }
-    }
+    }   
 }
